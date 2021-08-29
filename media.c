45b183a603e8ad0bdab8c1c59c5f478f3aa53155
Mayara Rocha <pr_lamayara@hotmail.com>
2021-08-28 23:38:02 -0300

    variavel number passou a ser nota

--- media.c
+++ media.c
@@ -2,16 +2,16 @@
 //Média aritmética entre três números.
 
 int main(void) {
-float number1, number2, number3, media;
+float nota1, nota2, nota3, media;
 
   printf("Informe o primeiro número\n");
-  scanf("%f", &number1);
+  scanf("%f", &nota1);
   printf("Informe o segundo número\n");
-  scanf("%f", &number2);
+  scanf("%f", &nota2);
   printf("Informe o terceiro número\n");
-  scanf("%f", &number3);
+  scanf("%f", &nota3);
 
-  media = ((number1 + number2 + number3) / 3);
+  media = ((nota1 + nota2 + nota3) / 3);
 
 printf("A média desses números é %f", media );
 
